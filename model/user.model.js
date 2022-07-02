a53eb59acdddf991cf9a84a25145c03438299096
Shahzad <mech1090@gmail.com>
2022-07-02 13:55:59 -0600

    model created

---
+++ model/user.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+},{
+    collection:'Practice 22 User'
+})
+
+const User = mongoose.model('User',userSchema)
+
+module.exports = User
